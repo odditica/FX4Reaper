5e2b7f4dbb32150eb16ba3fcb216a9c8e1f19869
Jan Vorisek <vorisek.jan@outlook.com>
2019-01-11 23:52:13 +0100

    Shell script now synchronises data between repo and Reaper folder. Added background scrolling to UI.

--- monologue/install.ps1
+++ monologue/install.ps1
@@ -1,1 +1,32 @@
-﻿cp -r monoctrl $env:APPDATA\REAPER\Effects\blokatt\
+﻿cd monoctrl
+
+$found = "$(find . -type f)"
+
+$found = $found.Split(" ")
+
+Foreach ($f in $found) {
+    echo "$f"
+    
+    $reaperPath = "$env:APPDATA\REAPER\Effects\blokatt\monoctrl"
+    $reaperFilePath = "$reaperPath\\$f"
+    Try
+    {
+        $repoDate = (Get-Item "$f").LastWriteTime
+        $reaperDate = (Get-Item "$reaperFilePath").LastWriteTime
+    }
+    Catch
+    {
+        continue;
+    }
+    if ("$repoDate" -gt "$reaperDate") {
+        echo "Newer in repo, copying."
+        cp "$f" "$reaperPath"
+    } elseif ("$repoDate" -lt "$reaperDate") {
+        echo "Newer in Reaper, copying."
+        cp "$reaperFilePath" .
+    }
+
+}
+#cp -r monoctrl $env:APPDATA\REAPER\Effects\blokatt\
+
+cd ..
